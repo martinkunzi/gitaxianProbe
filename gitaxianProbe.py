b73b33bf2a09ec694e942529302fe35ec9b5f736
unknown <martin.kunzi@he-arc.ch>
2016-05-09 00:48:45 +0200

    pseudo code version of the unifier, will be better tommorow early morning

--- gitaxianProbe.py
+++ gitaxianProbe.py
@@ -1,3 +1,15 @@
 """
 Welp, it will call every part of the program, one after another.
 """
+import cv2
+
+if __name__ == "__main__":
+    from system import argv
+    if len(argv) is 2:
+        redressedimage = bastienmain(argv[1])
+        imagename = quentinmain(redressedimage, clf)
+		imageextension = martinmain(redressedimage)
+		cv2.showimage(imagename + "," + imageextension, redressedimage)
+		cv2.waitforkey(0)
+	else:
+	    print("number of arguments incorrect. Should only be a path to an image")
